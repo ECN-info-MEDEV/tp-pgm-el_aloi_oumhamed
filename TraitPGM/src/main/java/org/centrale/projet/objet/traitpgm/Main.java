8f8984104601d7d2191637f64b853d86991597b0
Aloi <elaloi.otmane@gmail.com>
2021-12-02 11:36:14 +0100

    add histogram

--- TraitPGM/src/main/java/org/centrale/projet/objet/traitpgm/Main.java
+++ TraitPGM/src/main/java/org/centrale/projet/objet/traitpgm/Main.java
@@ -5,12 +5,51 @@
  */
 package org.centrale.projet.objet.traitpgm;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.StringTokenizer;
+
 /**
  *
  * @author Otmane
  */
 public class Main {
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        String filePath = "brain.pgm";
+        Reader reader = new FileReader(filePath);
+        BufferedReader br = new BufferedReader(reader);
+
+        br.readLine();
+        br.readLine();
+        
+        StringTokenizer sta = new StringTokenizer(br.readLine(), " ");
+        
+        int picWidth = Integer.parseInt(sta.nextToken());
+        int picHeight = Integer.parseInt(sta.nextToken());
+        int maxvalue = Integer.parseInt(br.readLine());
+        String line = br.readLine();
+        
+        // Histogram
+        int[] pixles = new int[256];
+        while(line !=null){
+            StringTokenizer st = new StringTokenizer(line, " ");
+            while(st.hasMoreTokens()){
+                  int pixel =Integer.parseInt(st.nextToken());
+                  pixles[pixel]++;
+                }
+            line = br.readLine();
+        }
+        
+//        for(int i=0;i<255;i++){
+//            System.out.print(pixles[i]);
+//        }
+             
+
+
     }
 }
